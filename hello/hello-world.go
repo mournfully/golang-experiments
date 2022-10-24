788f1569143ac51f3df4116c29ea04cf5e8f9969
local <minuramolligoda.dev@gmail.com>
2022-10-23 21:49:09 -0400

    constants

--- hello/hello-world.go
+++ hello/hello-world.go
@@ -4,10 +4,10 @@
 
 package main
 
-import {
+import (
 	"fmt"
 	"math"
-}
+)
 
 func main() {
 	// substrings can be added together with '+'
@@ -18,9 +18,20 @@ func main() {
 	fmt.Println("8.0/4.0 =", 8.0/4.0)
 	fmt.Println(true || false)
 
-	// variables, I guess no more `let`
+	// variables, var <name> <type> = <value>
+	// strings, integers, floats, booleans
 	var a = "initial"
-	f := "apple" // `:=` is shorthand for var
+	f := "apple" // `:=` shorthand for `var string`
+	var e int    // uninitialized = zero-valued
 	var b, c int = 1, 2
-	var e int // defaults to 0
+	fmt.Printf("strings: %s %s | uninitialized: %d | integers: %d %d\n", a, f, e, b, c)
+	// doesn't make a new line btw ^ so add \n
+
+	// constants, how is this different from variables?
+	const s string = "constant"
+	const n = 500
+	const d = 3e20 / n
+	fmt.Println(d) // NUMERIC constants by default have no type
+	fmt.Println(int64(d))
+	fmt.Println(math.Sin(n))
 }
