6799e1b5b36010f22a72ac608ad6c6a8e3da96a2
local <minuramolligoda.dev@gmail.com>
2022-10-23 20:57:25 -0400

    values

--- hello/hello-world.go
+++ hello/hello-world.go
@@ -1,10 +1,20 @@
+// https://gobyexample.com/hello-world
+// use `go run` to run a program
+// use `go build` to build the binary
+
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
-}
+	// substrings can be added together with '+'
+	fmt.Println("hello" + "go" + "lang")
 
-// use `go run` to run a program
-// use `go build` to build the binary
+	// fairly standard integers, floats, and booleans.
+	fmt.Println("6+9 =", 6+9)
+	fmt.Println("8.0/4.0 =", 8.0/4.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
